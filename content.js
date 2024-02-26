ebe8e3963b1c566e32f920834f346fa6253fa2bf
Mário Viegas <marioviegas2001@gmail.com>
2024-02-26 20:34:44 +0000

    Displays data in a proper form

--- content.js
+++ content.js
@@ -1,12 +1,20 @@
-// Find all h1 and p elements and retrieve their text content
-const headings = Array.from(document.querySelectorAll('h1')).map(h => h.textContent);
-const paragraphs = Array.from(document.querySelectorAll('p')).map(p => p.textContent);
+// Extract title
+const title = document.querySelector('.headline').textContent.trim();
 
-// Send the extracted text content back to the background script
+// Extract description
+const descriptionElement = document.querySelector('.story__blurb p');
+const description = descriptionElement ? descriptionElement.textContent.trim() : '';
+
+// Extract article content
+const storyBodyElement = document.querySelector('.story__body');
+const articleContent = storyBodyElement ? storyBodyElement.innerHTML : '';
+
+// Send the extracted information back to the background script
 chrome.runtime.sendMessage({
   action: 'extractedContent',
   data: {
-    headings: headings,
-    paragraphs: paragraphs
+    title: title,
+    description: description,
+    articleContent: articleContent
   }
 });
--- popup.js
+++ popup.js
@@ -2,15 +2,15 @@ chrome.runtime.sendMessage({ action: 'getExtractedData' }, (response) => {
   if (response && response.data) {
     const extractedData = response.data;
     const textFromHTML = document.getElementById('textFromHTML');
-    let htmlContent = '<h2>Extracted Headings:</h2>';
-    extractedData.headings.forEach((heading) => {
-      htmlContent += `<p>${heading}</p>`;
-    });
+    let htmlContent = '<h2>Title</h2>';
+    htmlContent += `<p>${extractedData.title}</p>`;
 
-    htmlContent += '<h2>Extracted Paragraphs:</h2>';
-    extractedData.paragraphs.forEach((paragraph) => {
-      htmlContent += `<p>${paragraph}</p>`;
-    });
+    htmlContent += '<h2>Description:</h2>';
+    htmlContent += `<p>${extractedData.description}</p>`;
+
+    htmlContent += '<h2>Article:</h2>';
+    htmlContent += `<p>${extractedData.articleContent}</p>`;
+    
 
     // Set the HTML content to the div
     textFromHTML.innerHTML = htmlContent;
