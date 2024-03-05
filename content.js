eb0417469195bcdfcd95f02b65934e341866591e
Mário Viegas <marioviegas2001@gmail.com>
2024-03-05 20:03:31 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,20 +1,37 @@
-// Extract title
-const title = document.querySelector('.headline').textContent.trim();
-
-// Extract description
-const descriptionElement = document.querySelector('.story__blurb p');
-const description = descriptionElement ? descriptionElement.textContent.trim() : '';
-
-// Extract article content
-const storyBodyElement = document.querySelector('.story__body');
-const articleContent = storyBodyElement ? storyBodyElement.innerHTML : '';
-
-// Send the extracted information back to the background script
-chrome.runtime.sendMessage({
-  action: 'extractedContent',
-  data: {
-    title: title,
-    description: description,
-    articleContent: articleContent
+// Define the JSON data within the extension's code
+const websiteSelectors = {
+  "www.publico.pt": {
+    "titleSelector": ".headline",
+    "descriptionSelector": ".story__blurb p",
+    "articleContentSelector": ".story__body"
   }
-});
+  //Add more websites and their respective selectors
+};
+
+// Extract selectors based on current website domain
+const currentWebsite = window.location.hostname;
+const selectors = websiteSelectors[currentWebsite];
+if (selectors) {
+  // Extract title
+  const title = document.querySelector(selectors.titleSelector)?.textContent.trim() || '';
+
+  // Extract description
+  const descriptionElement = document.querySelector(selectors.descriptionSelector);
+  const description = descriptionElement ? descriptionElement.textContent.trim() : '';
+
+  // Extract article content
+  const storyBodyElement = document.querySelector(selectors.articleContentSelector);
+  const articleContent = storyBodyElement ? storyBodyElement.innerHTML : '';
+
+  // Send the extracted information back to the background script
+  chrome.runtime.sendMessage({
+    action: 'extractedContent',
+    data: {
+      title: title,
+      description: description,
+      articleContent: articleContent
+    }
+  });
+} else {
+  console.log('Selectors for current website not found.');
+}
