44d732b0e4060b7d19f299e244eedbe93ce938c3
Mário Viegas <marioviegas2001@gmail.com>
2024-03-18 11:11:28 +0000

    Tabs disabled outside of allowed domains

--- background.js
+++ background.js
@@ -1,13 +1,30 @@
-/* *********************************************************
-* Receive data from content.js and send it to popup.js     *
-************************************************************/
-let extractedData = null;
+// Define an array of allowed domains
+const allowedDomains = ["www.publico.pt"];
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'extractedContent') {
-    extractedData = message.data;
-  } else if (message.action === 'getExtractedData') {
-    // Send the extracted data to the popup script
-    sendResponse({ data: extractedData });
+// Listen for changes in the active tab
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    // Get information about the activated tab
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        if (tab && isAllowedDomain(tab.url)) {
+            console.log("Current tab:", tab);
+            chrome.action.enable(tab.id);
+        } else {
+            chrome.action.disable(tab.id);
+            console.log("Not on an allowed domain.");
+        }
+    });
+});
+
+// Listen for changes in the reloaded tabs
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete" && !isAllowedDomain(tab.url)) {
+      chrome.action.disable(tabId);
   }
 });
+
+// Function to check if a given URL belongs to an allowed domain
+function isAllowedDomain(url) {
+    const domain = new URL(url).hostname;
+    console.log("Domain:", domain);
+    return allowedDomains.includes(domain);
+}
